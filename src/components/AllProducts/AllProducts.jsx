e0a3f637bb6ffb9731376d0e76bf330b6c179df7
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-10-31 23:31:23 +0600

    Navbar and DaisyUI added for design

---
+++ src/components/AllProducts/AllProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllProducts = () => {
+    return (
+        <div>
+            all AllProducts
+        </div>
+    );
+};
+
+export default AllProducts;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayouts from "./layouts/RootLayouts";
+import Home from "./components/Home/Home";
+import AllProducts from "./components/AllProducts/AllProducts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayouts,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path:'/allProducts',
+        Component: AllProducts
+      },
+      {
+
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
